083e448c8467ba9e424b687f9934284c7c3f287e
Mark Konakov <konakovmark@yandex.ru>
2021-10-11 10:34:37 +0300

    Create check_for_redirect function

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import os
 import requests
 
-base_url = 'https://tululu.org/txt.php?id='
+
+books_base_url = 'https://tululu.org/txt.php?id='
 books_path = os.path.join(os.getcwd(), 'books')
 os.makedirs(books_path, exist_ok=True)
 
 
-def load_books(url, books_count=11):
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+
+
+def load_books(books_count=11):
     for i in range(1, books_count):
-        url = f'{base_url}{i}'
+        url = f'{books_base_url}{i}'
         response = requests.get(url, verify=True)
-        if response:
+        try:
+            check_for_redirect(response)
             with open(f'{books_path}/id{i}', 'w') as f:
                 f.write(response.text)
+        except requests.HTTPError:
+            continue
 
 
+load_books()
 print(sorted(os.listdir(books_path)))
