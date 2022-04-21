a89f9dbef1d1e42b6195a28d789a5217d1f06f49
konakov-ds <konakov-ds@yandex.ru>
2022-04-21 21:34:05 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -1,13 +1,13 @@
 import json
 import os
+from pathlib import Path
 from urllib.parse import urljoin
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
-BOOKS_DIR = './dest/'
+BOOKS_DIR = '../dest/'
 
 
 def process_book_info(books_info='books_info.json'):
@@ -23,51 +23,42 @@ def process_book_info(books_info='books_info.json'):
     books_paths = [
         book[book.find('_') + 1: -4] for book in books
     ]
-    print(books_paths)
     for book in books_info:
         img_path_index = None
         guid = book['guid']
         book['book_path'] = ''
         if guid in books_paths:
             book_path_index = books_paths.index(guid)
-            book['book_path'] = urljoin(
-                    urljoin(BOOKS_DIR, 'books/'),
-                    books[book_path_index]
-                )
+            book['book_path'] = f'{BOOKS_DIR}books/{books[book_path_index]}'
         if guid in books_images_paths:
             img_path_index = books_images_paths.index(guid)
-        book['img_src'] = urljoin(
-            urljoin(BOOKS_DIR, 'images/'),
-            '_nopic.gif'
-        )
+        book['img_src'] = f'./images/_nopic.gif'
         if img_path_index:
-            book['img_src'] = urljoin(
-                urljoin(BOOKS_DIR, 'images/'),
-                images[img_path_index]
-            )
+            book['img_src'] = f'./images/{images[img_path_index]}'
     return books_info
 
 
-def render():
-    books_info = process_book_info()
-    rows = chunked(books_info, 2)
-
+def render(books_per_page=10):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-
     template = env.get_template('template.html')
 
-    rendered_page = template.render(rows=rows)
+    books_info = process_book_info()
+    books_pages = list(chunked(books_info, books_per_page))
+    num_pages = len(books_pages)
+    Path('pages').mkdir(parents=True, exist_ok=True)
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for page, books in enumerate(books_pages, start=1):
+        rows = chunked(books, 2)
+        rendered_page = template.render(rows=rows, page=page, num_pages=num_pages)
+
+        with open(f'pages/index{page}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 render()
 server = Server()
 server.watch('template.html', render)
-server.serve(root='.')
-# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-# server.serve_forever()
+server.serve(root='./pages/', default_filename='index*.html')
