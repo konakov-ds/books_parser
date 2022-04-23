23c8dfc8962e0339d817eca262b48f8f6cbd21dd
konakov-ds <konakov-ds@yandex.ru>
2022-04-23 12:48:41 +0300

    refresh pages

--- render_website.py
+++ render_website.py
@@ -32,7 +32,7 @@ def process_book_info(books_info='books_info.json'):
             book['book_path'] = f'../dest/books/{books[book_path_index]}'
         if guid in books_images_paths:
             img_path_index = books_images_paths.index(guid)
-        book['img_src'] = f'../dest/images/_nopic.gif'
+        book['img_src'] = f'../dest/images/nopic.gif'
         if img_path_index:
             book['img_src'] = f'../dest/images/{images[img_path_index]}'
     return books_info
