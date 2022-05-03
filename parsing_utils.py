84903e721b44ce47a2d44fb287fe9808d254b06a
konakov-ds <konakov-ds@yandex.ru>
2022-05-03 13:38:30 +0300

    move get_books_info to utils

--- parsing_utils.py
+++ parsing_utils.py
@@ -1,3 +1,7 @@
+import json
+import os
+from pathlib import Path
+
 import requests
 import uuid
 from bs4 import BeautifulSoup
@@ -5,6 +9,7 @@ from urllib.parse import urljoin
 
 
 base_url = "https://tululu.org/l55/"
+CONTENT_DIR = '../dest/'
 
 
 def check_for_redirect(response):
@@ -48,3 +53,28 @@ def parse_book_page(book_id):
     }
 
     return book_info
+
+def get_books_info(books_info='books_info.json'):
+    images = os.listdir('./dest/images/')
+    books = os.listdir('./dest/books/')
+
+    with open(Path('./dest/', books_info)) as f:
+        books_info = json.load(f)
+
+    images_paths = [
+        image[:image.find('_')] for image in images
+    ]
+    books_paths = [
+        book[book.find('_') + 1: -4] for book in books
+    ]
+    for book in books_info:
+        guid = book['guid']
+        book['book_path'] = ''
+        if guid in books_paths:
+            book_path_index = books_paths.index(guid)
+            book['book_path'] = Path(CONTENT_DIR, 'books/', books[book_path_index])
+            book['img_src'] = Path(CONTENT_DIR, 'images/', 'nopic.gif')
+        if guid in images_paths:
+            img_path_index = images_paths.index(guid)
+            book['img_src'] = Path(CONTENT_DIR, 'images/', images[img_path_index])
+    return books_info
