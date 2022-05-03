b626f817bab2d2abe00d510cc73228f7f37b19d7
konakov-ds <konakov-ds@yandex.ru>
2022-05-03 12:32:54 +0300

    clean code in get_books func

--- render_website.py
+++ render_website.py
@@ -1,40 +1,39 @@
 import json
 import os
 from pathlib import Path
-from urllib.parse import urljoin
+
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
-BOOKS_DIR = './dest/'
+
+CONTENT_DIR = '../dest/'
 
 
-def process_book_info(books_info='books_info.json'):
-    images = os.listdir(Path(BOOKS_DIR, 'images/'))
-    books = os.listdir(Path(BOOKS_DIR, 'books/'))
+def get_books_info(books_info='books_info.json'):
+    images = os.listdir('./dest/images/')
+    books = os.listdir('./dest/books/')
 
-    with open(Path(BOOKS_DIR, books_info)) as f:
+    with open(Path('./dest/', books_info)) as f:
         books_info = json.load(f)
 
-    books_images_paths = [
-        book[:book.find('_')] for book in images
+    images_paths = [
+        image[:image.find('_')] for image in images
     ]
     books_paths = [
         book[book.find('_') + 1: -4] for book in books
     ]
     for book in books_info:
-        img_path_index = None
         guid = book['guid']
         book['book_path'] = ''
         if guid in books_paths:
             book_path_index = books_paths.index(guid)
-            book['book_path'] = f'../dest/books/{books[book_path_index]}'
-        if guid in books_images_paths:
-            img_path_index = books_images_paths.index(guid)
-        book['img_src'] = f'../dest/images/nopic.gif'
-        if img_path_index:
-            book['img_src'] = f'../dest/images/{images[img_path_index]}'
+            book['book_path'] = Path(CONTENT_DIR, 'books/', books[book_path_index])
+            book['img_src'] = Path(CONTENT_DIR, 'images/', 'nopic.gif')
+        if guid in images_paths:
+            img_path_index = images_paths.index(guid)
+            book['img_src'] = Path(CONTENT_DIR, 'images/', images[img_path_index])
     return books_info
 
 
@@ -45,7 +44,7 @@ def render(books_per_page=10):
     )
     template = env.get_template('template.html')
 
-    books_info = process_book_info()
+    books_info = get_books_info()
     books_pages = list(chunked(books_info, books_per_page))
     num_pages = len(books_pages)
     Path('pages').mkdir(parents=True, exist_ok=True)
